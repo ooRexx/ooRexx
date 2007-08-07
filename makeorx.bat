c4275de7b801e09cb760ad4767e37cf4d85c4a8a
miesfeld <miesfeld@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2007-08-07 16:18:40 +0000

    Implement RFE [ 1769499 ] Improve Windows build process to check for required doc
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/interpreter-3.x/trunk@659 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- makeorx.bat
+++ makeorx.bat
@@ -34,6 +34,11 @@
 @REM SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 @REM
 @ECHO OFF
+
+REM Note: This batch file will not work if command extensions are disabled.
+REM       Command extensions are enabled by default in W2K, XP, W2K3, and Vista
+REM       They are probably enabled in NT.
+
 REM Note: needed for XCOPY command under Win 2000 to suppress the overwrite question
 REM Win NT 4.0 ignores this environment variable
 SET COPYCMD=/Y
@@ -41,10 +46,13 @@ SET COPYCMD=/Y
 REM MHES 29122004 Added set CPU=ix86
 set CPU=ix86
 
+REM Check that we have at least the first option
+IF %1x == x GOTO HELP
+
 REM Check for the 'package' option
 if %2x == x (
   SET DOPACKAGE=0
-  GOTO SKIP
+  GOTO DOC_CHECK_DONE
 )
 
 if %2 == PACKAGE (
@@ -53,24 +61,42 @@ if %2 == PACKAGE (
   SET PACKAGE_DBG=0
 ) ELSE GOTO HELP
 
-:SKIP
+REM  The package step is being done, check for the docs.
+goto DOC_CHECK
+
+:DOC_CHECK_DONE
 
 REM Check that SRC_DIR and SRC_DRV are set
 IF %SRC_DRV%x == x GOTO HELP_SRC_DRV
 IF %SRC_DIR%x == x GOTO HELP_SRC_DRV
 
 REM Check for the type of build
-IF %1x == x GOTO HELP
+:CHECK_BUILD_TYPE
 IF %1 == NODEBUG GOTO NODEBUG
 IF %1 == DEBUG GOTO DEBUG
 IF %1 == BOTH GOTO BOTH
 
 :HELP
-ECHO Syntax: makeorx BUILD_TYPE [PACKAGE]
+ECHO Syntax: makeorx BUILD_TYPE [PACKAGE] [DOC_LOCATION]
 ECHO Where BUILD_TYPE is required and exactly one of DEBUG NODEBUG BOTH
 ECHO Where PACKAGE is optional.  If present and exactly PACKAGE the
 ECHO Windows ooRexx install package will be built.
-GOTO END
+ECHO.
+ECHO If creating the install package, the ooRexx PDF documentation must be
+ECHO located in the doc subdirectory of the root build directory.  If it is
+ECHO not, an attempt will be made to copy it from the directory specified by
+ECHO the third optional argument: DOC_LOCATION.  Note that alternatively,
+ECHO DOC_LOCATION can be specified as an environment variable.  I.e.,
+ECHO.
+ECHO set DOC_LOCATION=C:\myDocs
+ECHO makeorx NODEBUG PACKAGE
+ECHO.
+ECHO and
+ECHO.
+ECHO makeorx NODEBUG PACKAGE C:\myDocs
+ECHO.
+ECHO are equivalent commands.
+GOTO ENV_VARS_CLEANUP
 
 :HELP_SRC_DRV
 ECHO *==============================================================
@@ -81,7 +107,7 @@ ECHO e.g.
 ECHO "SET SRC_DRV=F:"
 ECHO "SET SRC_DIR=\oorexx\interpreter_3x"
 ECHO *======================================================
-GOTO END
+GOTO ENV_VARS_CLEANUP
 
 :NODEBUG
 SET MKNODEBUG=1
@@ -123,7 +149,7 @@ SET MKDEBUG=0
 killer rxapi.exe
 CALL ORXDB %BLDRELEASE%
 
-IF ERRORLEVEL 1 GOTO PACKAGE_CLEANUP
+IF ERRORLEVEL 1 GOTO ENV_VARS_CLEANUP
 
 REM Make sure we are back in the root build directory.
 CD %SRC_DRV%%SRC_DIR%
@@ -138,10 +164,7 @@ IF %DOPACKAGE% == 1 (
     IF %%i == ORX_MINOR SET MINOR_NUM=%%j
     IF %%i == ORX_MOD_LVL SET LVL_NUM=%%j
   )
-) ELSE (
-  SET DOPACKAGE=
-  GOTO END
-)
+) ELSE GOTO ENV_VARS_CLEANUP
 
 IF %MAJOR_NUM%x == x GOTO SET_FAILED
 SET NODOTS=%MAJOR_NUM%%MINOR_NUM%%LVL_NUM%
@@ -162,8 +185,8 @@ ren ooRexx%NODOTS%.exe ooRexx%NODOTS%-debug.exe
 move ooRexx%NODOTS%-debug.exe ..\..\
 cd ..\..\
 
-REM If not making the release version skip to package clean up.
-IF %PACKAGE_REL% == 0 GOTO PACKAGE_CLEANUP
+REM If not making the release version skip to environment variables clean up.
+IF %PACKAGE_REL% == 0 GOTO ENV_VARS_CLEANUP
 
 :PACKAGE_RELEASE
 SET BINDIR=/DBINDIR=%SRC_DRV%%SRC_DIR%\Win32Rel
@@ -172,7 +195,8 @@ makensis %DOTVER% %NODOTVER% %SRCDIR% %BINDIR% oorexx.nsi
 move ooRexx%NODOTS%.exe ..\..\
 cd ..\..\
 
-:PACKAGE_CLEANUP
+:ENV_VARS_CLEANUP
+SET CPU=
 SET DOPACKAGE=
 SET PACKAGE_REL=
 SET PACKAGE_DBG=
@@ -184,13 +208,87 @@ SET DOTVER=
 SET NODOTVER=
 SET SRCDIR=
 SET BINDIR=
+SET MISSING_DOC=
 
 GOTO END
 
 :SET_FAILED
 ECHO Could not set the package major minor numbers.
 ECHO Skipping the installer package step.
+GOTO ENV_VARS_CLEANUP
 
+REM - - - - - - - - - - - - END exits this batch file - - - - - - - - - - - - -
 :END
 REM Make sure we are back in the root build directory.
 CD %SRC_DRV%%SRC_DIR%
+exit /b
+
+REM - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+REM   Labels below this point handle some of the tedious chores for this
+REM   batch file.  They then use goto to return to a point of execution above.
+REM - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+:DOC_CHECK
+if not exist doc\nul md doc
+
+SET MISSING_DOC=0
+if not exist doc\readme.pdf   SET MISSING_DOC=1
+if not exist doc\rexxpg.pdf   SET MISSING_DOC=1
+if not exist doc\rexxref.pdf  SET MISSING_DOC=1
+if not exist doc\rxmath.pdf   SET MISSING_DOC=1
+if not exist doc\rxsock.pdf   SET MISSING_DOC=1
+if not exist doc\rxftp.pdf    SET MISSING_DOC=1
+if not exist doc\oodialog.pdf SET MISSING_DOC=1
+
+if %MISSING_DOC% EQU 0 goto DOC_CHECK_DONE
+
+REM  Missing some doc, try to copy it from a specified location.
+if %DOC_LOCATION%x == x (
+  if %3x == x (
+    ECHO The package option is specified, but some doc is missing and the
+    ECHO location to copy the doc from can not be determined.
+    ECHO.
+    GOTO HELP
+  ) else SET DOC_LOCATION=%3
+)
+
+if not exist doc\readme.pdf (
+ if not exist %DOC_LOCATION%\readme.pdf goto NO_DOC_ERR
+)
+copy %DOC_LOCATION%\readme.pdf doc 1>nul 2>&1
+
+if not exist doc\rexxpg.pdf (
+ if not exist %DOC_LOCATION%\rexxpg.pdf goto NO_DOC_ERR
+)
+copy %DOC_LOCATION%\rexxpg.pdf doc 1>nul 2>&1
+
+if not exist doc\rexxref.pdf (
+ if not exist %DOC_LOCATION%\rexxref.pdf goto NO_DOC_ERR
+)
+copy %DOC_LOCATION%\rexxref.pdf doc 1>nul 2>&1
+
+if not exist doc\rxmath.pdf (
+ if not exist %DOC_LOCATION%\rxmath.pdf goto NO_DOC_ERR
+)
+copy %DOC_LOCATION%\rxmath.pdf doc 1>nul 2>&1
+
+if not exist doc\rxsock.pdf (
+ if not exist %DOC_LOCATION%\rxsock.pdf goto NO_DOC_ERR
+)
+copy %DOC_LOCATION%\rxsock.pdf doc 1>nul 2>&1
+
+if not exist doc\rxftp.pdf (
+ if not exist %DOC_LOCATION%\rxftp.pdf goto NO_DOC_ERR
+)
+copy %DOC_LOCATION%\rxftp.pdf doc 1>nul 2>&1
+
+if not exist doc\oodialog.pdf (
+ if not exist %DOC_LOCATION%\oodialog.pdf goto NO_DOC_ERR
+)
+copy %DOC_LOCATION%\oodialog.pdf doc 1>nul 2>&1
+
+GOTO DOC_CHECK_DONE
+
+:NO_DOC_ERR
+ECHO Failed to locate some doc file(s) for the package option, aborting.
+GOTO ENV_VARS_CLEANUP
