005b3781564834b9af560d3b1788849b6f834985
miesfeld <miesfeld@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2010-10-08 17:28:39 +0000

    Fix error in bit manipulation
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/main/trunk@6244 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- extensions/platform/windows/oodialog/oodViewControls.cpp
+++ extensions/platform/windows/oodialog/oodViewControls.cpp
@@ -2275,7 +2275,7 @@ static uint32_t changeStyle(RexxMethodContext *c, pCDialogControl pCSelf, CSTRIN
     uint32_t newStyle = 0;
     if ( remove )
     {
-        newStyle &= ~listViewStyle(_style, 0);
+        newStyle = oldStyle & ~listViewStyle(_style, 0);
         if ( _additionalStyle != NULL )
         {
             newStyle = listViewStyle(_additionalStyle, newStyle);
