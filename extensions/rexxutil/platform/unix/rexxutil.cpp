2477964e92963e1ce516e0e9f492bba58910238c
wdashley <wdashley@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2010-11-12 20:07:18 +0000

    Fix the trunk so we consistently call stat64 in rexxutil.
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/main/trunk@6384 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- extensions/rexxutil/platform/unix/rexxutil.cpp
+++ extensions/rexxutil/platform/unix/rexxutil.cpp
@@ -552,14 +552,14 @@ int OpenFile(
    const char  *file,                  /* file name                  */
    GetFileData *filedata )             /* global file information    */
 {
-   struct stat finfo;                  /* file information           */
+   struct stat64 finfo;                /* file information           */
    char *      endptr = NULL;          /* end of buffer pointer      */
 
                                        /* try to open the file       */
   if((filedata->handle = fopen(file,"r")) == NULL)
     return (1);                        /* return failure             */
                                        /* retrieve the file size     */
-  if((stat(file,&finfo) == -1)||(!finfo.st_size)){
+  if((stat64(file,&finfo) == -1)||(!finfo.st_size)){
     fclose(filedata->handle);          /* close the file             */
     return (1);                        /* and quit                   */
   }
@@ -988,7 +988,7 @@ int LinFindNextFile(
   const char * filespec,               /* filespec to search for     */
   const char * path,                   /* current path               */
   DIR *dir_handle,                     /* directory handle           */
-  struct stat *finfo,                  /* return buf for the finfo   */
+  struct stat64 *finfo,                /* return buf for the finfo   */
   char * *d_name,                      /* name of the file found     */
   size_t caseless)                     /* case insensitive matching  */
 {
@@ -1003,7 +1003,7 @@ int LinFindNextFile(
 
                                        /* make full spec             */
     sprintf(full_path, "%s%s", path, dir_entry->d_name);
-    lstat(full_path, finfo);           /* read the info about it     */
+    lstat64(full_path, finfo);           /* read the info about it     */
 
   if(S_ISREG(finfo->st_mode) ||        /* if it is a file            */
      S_ISCHR(finfo->st_mode) ||        /* or a device special        */
@@ -1052,7 +1052,7 @@ int LinFindNextDir(
   const char * filespec,               /* filespec to search for     */
   const char * path,                   /* current path               */
   DIR *dir_handle,                     /* directory handle           */
-  struct stat *finfo,                  /* return buf for the finfo   */
+  struct stat64 *finfo,                /* return buf for the finfo   */
   char * *d_name,                      /* name of the file found     */
   size_t caseless)                     /* case insensitive matching  */
 {
@@ -1067,7 +1067,7 @@ int LinFindNextDir(
 
                                        /* make full spec             */
     sprintf(full_path, "%s%s", path, dir_entry->d_name);
-    lstat(full_path, finfo);           /* read the info about it     */
+    lstat64(full_path, finfo);           /* read the info about it     */
 
     if(S_ISDIR(finfo->st_mode)){       /* if it is a directory       */
 
@@ -1112,7 +1112,7 @@ size_t FormatFile(
                                        /* search for                 */
   int          *dmask,                 /* Mask of attributes to set  */
   size_t        options,               /* Search and output format   */
-  struct stat  *finfo )                /* File info sturcture        */
+  struct stat64 *finfo )              /* File info sturcture        */
 {
   struct tm *timestamp;                /* Time info about the file   */
   char tp;                             /* type of the entry          */
@@ -1277,12 +1277,12 @@ int  RecursiveFindFile(
   int        *smask,                   /* Mask of attributes to      */
                                        /* search for                 */
   int        *dmask,                   /* Mask of attributes to set  */
-  size_t       options )                /* Search and output format   */
+  size_t       options )               /* Search and output format   */
                                        /* options                    */
 {
   char  tempfile[MAX+1];               /* Used to hold temp file name*/
   DIR *dir_handle;                     /* Directory handle           */
-  struct stat finfo;                   /* file information           */
+  struct stat64 finfo;                 /* file information           */
   char * filename;
   size_t caseless = options&CASELESS;
 
@@ -3933,7 +3933,7 @@ size_t RexxEntry SysDumpVariables(const char *name, size_t numargs, CONSTRXSTRIN
 
 size_t RexxEntry SysGetFileDateTime(const char *name, size_t numargs, CONSTRXSTRING args[], const char *queuename, PRXSTRING retstr)
 {
-  struct    stat buf;
+  struct    stat64 buf;
   struct    tm *newtime;
   const char *dir_buf = NULL;            /* full directory path        */
   bool      fOk = true;
@@ -3953,7 +3953,7 @@ size_t RexxEntry SysGetFileDateTime(const char *name, size_t numargs, CONSTRXSTR
     dir_buf = args[0].strptr;
   }
 
-  if (stat(dir_buf, &buf) < 0)
+  if (stat64(dir_buf, &buf) < 0)
   {
      fOk = false;
   }
@@ -4019,7 +4019,7 @@ size_t RexxEntry SysSetFileDateTime(const char *name, size_t numargs, CONSTRXSTR
   struct utimbuf timebuf;
   struct tm *newtime;
   time_t ltime;
-  struct stat buf;
+  struct stat64 buf;
   const char *dir_buf = NULL;
   bool      alloc_Flag = false;
 
@@ -4042,7 +4042,7 @@ size_t RexxEntry SysSetFileDateTime(const char *name, size_t numargs, CONSTRXSTR
     dir_buf = args[0].strptr;
   }
 
-  if (stat(dir_buf, &buf) < 0)
+  if (stat64(dir_buf, &buf) < 0)
   {
      fOk =  false;
   }
@@ -4948,9 +4948,9 @@ RexxRoutine2(int, SysFileMove, CSTRING, fromFile, CSTRING, toFile)
 
 RexxRoutine1(logical_t, SysIsFile, CSTRING, filename)
 {
-    struct stat finfo;                   /* return buf for the finfo   */
+    struct stat64 finfo;                   /* return buf for the finfo   */
 
-    int rc = stat(filename, &finfo);     /* read the info about it     */
+    int rc = stat64(filename, &finfo);     /* read the info about it     */
     // check the flag settings for a regular file
     return rc == 0 && (S_ISREG(finfo.st_mode) || S_ISBLK(finfo.st_mode));
 }
@@ -4967,9 +4967,9 @@ RexxRoutine1(logical_t, SysIsFile, CSTRING, filename)
 
 RexxRoutine1(logical_t, SysIsFileDirectory, CSTRING, filename)
 {
-    struct stat finfo;                   /* return buf for the finfo   */
+    struct stat64 finfo;                   /* return buf for the finfo   */
 
-    int rc = stat(filename, &finfo);     /* read the info about it     */
+    int rc = stat64(filename, &finfo);     /* read the info about it     */
     return rc == 0 && S_ISDIR(finfo.st_mode);
 }
 
@@ -4985,9 +4985,9 @@ RexxRoutine1(logical_t, SysIsFileDirectory, CSTRING, filename)
 
 RexxRoutine1(logical_t, SysIsFileLink, CSTRING, filename)
 {
-    struct stat finfo;                   /* return buf for the finfo   */
+    struct stat64 finfo;                   /* return buf for the finfo   */
 
-    int rc = lstat(filename, &finfo);       /* read the info about it     */
+    int rc = lstat64(filename, &finfo);    /* read the info about it     */
     return rc == 0 && S_ISLNK(finfo.st_mode);
 }
 
@@ -5003,9 +5003,9 @@ RexxRoutine1(logical_t, SysIsFileLink, CSTRING, filename)
 
 RexxRoutine1(logical_t, SysFileExists, CSTRING, filename)
 {
-    struct stat finfo;                   /* return buf for the finfo   */
+    struct stat64 finfo;                   /* return buf for the finfo   */
 
-    return stat(filename, &finfo) == 0; /* read the info about it     */
+    return stat64(filename, &finfo) == 0; /* read the info about it     */
 }
 
 
