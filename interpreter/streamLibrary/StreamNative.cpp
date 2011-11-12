c08b7f032c88eea140462fee21b64f853b007e8d
jfaucher <jfaucher@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2011-11-12 19:42:19 +0000

    As discussed on the dev list, commit a possible fix for bug 3274050, to be adjusted as needed.
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/main/trunk@7289 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- interpreter/streamLibrary/StreamNative.cpp
+++ interpreter/streamLibrary/StreamNative.cpp
@@ -632,24 +632,27 @@ const char *StreamInfo::handleOpen(const char *options)
             // position at the end, and set the write position
             setPosition(size(), charWritePosition);
 
-            char   char_buffer;
-            size_t bytesRead;
-            // read the last character of the buffer.
-            // we don't call readBuffer() for this because it
-            // moves the read pointer
-            if (!fileInfo.read(&char_buffer, 1, bytesRead))
+            if (!write_only)
             {
-                notreadyError();
-            }
+                char   char_buffer;
+                size_t bytesRead;
+                // read the last character of the buffer.
+                // we don't call readBuffer() for this because it
+                // moves the read pointer
+                if (!fileInfo.read(&char_buffer, 1, bytesRead))
+                {
+                    notreadyError();
+                }
 
-            // if the last character is not a ctrl_z, we need to
-            // step past it.
-            if (ctrl_z != char_buffer)
-            {
-                charWritePosition++;
-                /* error on Windows so we had to put in that */
-                /* explicitly set the position       */
-                setPosition(charWritePosition, charWritePosition);
+                // if the last character is not a ctrl_z, we need to
+                // step past it.
+                if (ctrl_z != char_buffer)
+                {
+                    charWritePosition++;
+                    /* error on Windows so we had to put in that */
+                    /* explicitly set the position       */
+                    setPosition(charWritePosition, charWritePosition);
+                }
             }
         }
         lineWritePosition = 0;
@@ -793,24 +796,27 @@ void StreamInfo::implicitOpen(int type)
             // position at the end, and set the write position
             setPosition(size(), charWritePosition);
 
-            char   char_buffer;
-            size_t bytesRead;
-            // read the last character of the buffer.
-            // we don't call readBuffer() for this because it
-            // moves the read pointer
-            if (!fileInfo.read(&char_buffer, 1, bytesRead))
+            if (!write_only)
             {
-                notreadyError();
-            }
+                char   char_buffer;
+                size_t bytesRead;
+                // read the last character of the buffer.
+                // we don't call readBuffer() for this because it
+                // moves the read pointer
+                if (!fileInfo.read(&char_buffer, 1, bytesRead))
+                {
+                    notreadyError();
+                }
 
-            // if the last character is not a ctrl_z, we need to
-            // step past it.
-            if (ctrl_z != char_buffer)
-            {
-                charWritePosition++;
-                /* error on Windows so we had to put in that */
-                /* explicitly set the position       */
-                setPosition(charWritePosition, charWritePosition);
+                // if the last character is not a ctrl_z, we need to
+                // step past it.
+                if (ctrl_z != char_buffer)
+                {
+                    charWritePosition++;
+                    /* error on Windows so we had to put in that */
+                    /* explicitly set the position       */
+                    setPosition(charWritePosition, charWritePosition);
+                }
             }
         }
         // set default line positioning
@@ -2384,7 +2390,8 @@ const char *StreamInfo::streamOpen(const char *options)
     {
         // if this is some sort of device, it might be output only (i.e., a
         // printer).  Try opening again write only
-        if (fileInfo.isDevice())
+        // bug 3274050 : no longer limited to device, a regular file can have write-only permissions
+        if (write_only || fileInfo.isDevice())
         {
             if (!open(WR_CREAT, S_IWRITE, shared))
             {
@@ -2428,24 +2435,27 @@ const char *StreamInfo::streamOpen(const char *options)
             // position at the end, and set the write position
             setPosition(size(), charWritePosition);
 
-            char   char_buffer;
-            size_t bytesRead;
-            // read the last character of the buffer.
-            // we don't call readBuffer() for this because it
-            // moves the read pointer
-            if (!fileInfo.read(&char_buffer, 1, bytesRead))
+            if (!write_only)
             {
-                notreadyError();
-            }
+                char   char_buffer;
+                size_t bytesRead;
+                // read the last character of the buffer.
+                // we don't call readBuffer() for this because it
+                // moves the read pointer
+                if (!fileInfo.read(&char_buffer, 1, bytesRead))
+                {
+                    notreadyError();
+                }
 
-            // if the last character is not a ctrl_z, we need to
-            // step past it.
-            if (ctrl_z != char_buffer)
-            {
-                charWritePosition++;
-                /* error on Windows so we had to put in that */
-                /* explicitly set the position       */
-                setPosition(charWritePosition, charWritePosition);
+                // if the last character is not a ctrl_z, we need to
+                // step past it.
+                if (ctrl_z != char_buffer)
+                {
+                    charWritePosition++;
+                    /* error on Windows so we had to put in that */
+                    /* explicitly set the position       */
+                    setPosition(charWritePosition, charWritePosition);
+                }
             }
         }
         /* set default line positioning      */
