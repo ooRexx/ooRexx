092371f97e1c97d036c0e949a68d17aff819d636
wdashley <wdashley@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2014-05-03 18:54:49 +0000

    Fixed a few more problems with the generation of the config.h file.
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/main/trunk@10006 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- config.h.in.cmake
+++ config.h.in.cmake
@@ -190,29 +190,29 @@ Fldr/* config.h.in.  Generated from CMake.  */
 #cmakedefine HAVE_VPRINTF
 
 /* Name of package */
-#cmakedefine PACKAGE
+#define PACKAGE ${PACKAGE}
 
 /* Define to the address where bug reports for this package should be sent. */
-#cmakedefine PACKAGE_BUGREPORT
+#define PACKAGE_BUGREPORT ${PACKAGE_BUGREPORT}
 
 /* Define to the full name of this package. */
-#cmakedefine PACKAGE_NAME
+#define PACKAGE_NAME $PACKAGE_NAME}
 
 /* Define to the full name and version of this package. */
-#cmakedefine PACKAGE_STRING
+#define PACKAGE_STRING ${PACKAGE_STRING}
 
 /* Define to the one symbol short name of this package. */
-#cmakedefine PACKAGE_TARNAME
+#define PACKAGE_TARNAME ${PACKAGE_TARNAME}
 
 /* Define to the home page for this package. */
-#cmakedefine PACKAGE_URL
+#define PACKAGE_URL ${PACKAGE_URL}
 
 /* Define to the version of this package. */
-#cmakedefine PACKAGE_VERSION
+#define PACKAGE_VERSION ${PACKAGE_VERSION}
 
 /* Define to 1 if you have the ANSI C header files. */
-#cmakedefine STDC_HEADERS
+#cmakedefine STDC_HEADERS ${STDC_HEADERS}
 
 /* Version number of package */
-#cmakedefine VERSION
+#cmakedefine VERSION ${VERSION}
 
