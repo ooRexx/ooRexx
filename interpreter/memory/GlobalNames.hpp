4b575bb01294864e31ba1f1c5f19ef2570800859
bigrixx <bigrixx@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2014-09-19 21:56:57 +0000

    Missed file on the merge
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/main/trunk@10465 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

---
+++ interpreter/memory/GlobalNames.hpp
@@ -0,0 +1,60 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/* Copyright (c) 1995, 2004 IBM Corporation. All rights reserved.             */
+/* Copyright (c) 2005-2014 Rexx Language Association. All rights reserved.    */
+/*                                                                            */
+/* This program and the accompanying materials are made available under       */
+/* the terms of the Common Public License v1.0 which accompanies this         */
+/* distribution. A copy is also available at the following address:           */
+/* http://www.oorexx.org/license.html                                         */
+/*                                                                            */
+/* Redistribution and use in source and binary forms, with or                 */
+/* without modification, are permitted provided that the following            */
+/* conditions are met:                                                        */
+/*                                                                            */
+/* Redistributions of source code must retain the above copyright             */
+/* notice, this list of conditions and the following disclaimer.              */
+/* Redistributions in binary form must reproduce the above copyright          */
+/* notice, this list of conditions and the following disclaimer in            */
+/* the documentation and/or other materials provided with the distribution.   */
+/*                                                                            */
+/* Neither the name of Rexx Language Association nor the names                */
+/* of its contributors may be used to endorse or promote products             */
+/* derived from this software without specific prior written permission.      */
+/*                                                                            */
+/* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS        */
+/* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT          */
+/* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS          */
+/* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT   */
+/* OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,      */
+/* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED   */
+/* TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,        */
+/* OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY     */
+/* OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING    */
+/* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS         */
+/* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.               */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+/******************************************************************************/
+/*                                                                            */
+/* A namespace definition of all Global Name objects.                         */
+/* All Name object are addressible via OREF_* names.  The actual names are    */
+/* defined in the GlobalNames.h file, which is included multiple times with   */
+/* suitable defines of the GLOBAL_NAME definition to perform operaitons on    */
+/* the names.  This file defines all of the strings in a global namespace.    */
+/*                                                                            */
+/******************************************************************************/
+#ifndef GlobalNames_Included
+#define GlobalNames_Included
+
+#undef GLOBAL_NAME
+#define GLOBAL_NAME(name, value) extern RexxString *name;
+
+namespace GlobalNames
+{
+// pull in the global name definitions
+#include "GlobalNames.h"
+
+}
+
+#endif
