5ed91b975121cd855327e0f29ef3ff02a15c702e
bigrixx <bigrixx@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2014-10-05 23:24:19 +0000

    [#1271] SYSSTEMCOPY incorrect result on v4.2.0 (was OK on v4.1.2)
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/main/trunk@10622 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- extensions/rexxutil/platform/unix/rexxutil.cpp
+++ extensions/rexxutil/platform/unix/rexxutil.cpp
@@ -6,7 +6,7 @@
 /* This program and the accompanying materials are made available under       */
 /* the terms of the Common Public License v1.0 which accompanies this         */
 /* distribution. A copy is also available at the following address:           */
-/* http://www.oorexx.org/license.html                          */
+/* http://www.oorexx.org/license.html                                         */
 /*                                                                            */
 /* Redistribution and use in source and binary forms, with or                 */
 /* without modification, are permitted provided that the following            */
@@ -5222,7 +5222,7 @@ RexxRoutine6(int, SysStemCopy, RexxStemObject, fromStem, RexxStemObject, toStem,
                 // return this as a failure
                 return -1;
             }
-            context->SetStemArrayElement(toStem, index, value);
+            context->SetStemArrayElement(toStem, index + fromCount, value);
         }
 
 
