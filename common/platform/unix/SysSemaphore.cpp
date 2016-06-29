8da12ac04a0e44341ac0e231ede9c1d9289daa55
rvjansen <rvjansen@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2016-06-29 17:31:07 +0000

    fix for CLOCK_REALTIME on MacOSX
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/main/trunk@11077 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- common/platform/unix/SysSemaphore.cpp
+++ common/platform/unix/SysSemaphore.cpp
@@ -59,6 +59,21 @@
     #include <sched.h>
 #endif
 
+
+#ifdef __MACH__
+#define CLOCK_REALTIME 1
+#include <sys/time.h>
+//clock_gettime is not implemented on OSX
+int clock_gettime(int /*clk_id*/, struct timespec* t) {
+    struct timeval now;
+    int rv = gettimeofday(&now, NULL);
+    if (rv) return rv;
+    t->tv_sec  = now.tv_sec;
+    t->tv_nsec = now.tv_usec * 1000;
+    return 0;
+}
+#endif
+
 #include <errno.h>
 
 #include "SysSemaphore.hpp"
