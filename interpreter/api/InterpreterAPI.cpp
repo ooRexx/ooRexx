7c79d27f3c61046492fc721a14365bf6d986d47a
erich_st <erich_st@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2017-05-18 15:45:37 +0000

    fix code for [bugs:#1451] CreateRexxInterpreter returns 1002 instead of 1 on success
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/main/trunk@11251 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- interpreter/api/InterpreterAPI.cpp
+++ interpreter/api/InterpreterAPI.cpp
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /* Copyright (c) 1995, 2004 IBM Corporation. All rights reserved.             */
-/* Copyright (c) 2005-2014 Rexx Language Association. All rights reserved.    */
+/* Copyright (c) 2005-2017 Rexx Language Association. All rights reserved.    */
 /*                                                                            */
 /* This program and the accompanying materials are made available under       */
 /* the terms of the Common Public License v1.0 which accompanies this         */
@@ -368,15 +368,13 @@ RexxReturnCode REXXENTRY RexxResetTrace(process_id_t procid, thread_id_t threadi
  *
  * @param instance The returned instance pointer.
  * @param context  The initial thread context for this instance.
- * @param exits    A list of user exits to be used for this context.
- * @param userData A pointer to a user defined data area.
+ * @param options  An array of interpreter instance options.
  *
- * @return RXAPI_OK if the instance was created, RXAPI_MEMFAIL for any
- *         creation errors.
+ * @return 1 if the instance was created, 0 for any creation errors.
  */
 RexxReturnCode RexxEntry RexxCreateInterpreter(RexxInstance **instance, RexxThreadContext **context, RexxOption *options)
 {
-    return Interpreter::createInstance(*instance, *context, options) ? RXAPI_OK : RXAPI_MEMFAIL;
+    return Interpreter::createInstance(*instance, *context, options) ? 0 : 1;
 }
 
 /**
