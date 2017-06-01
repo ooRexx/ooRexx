65c80fd2d2c68c5a5fb3678578311dd120939073
erich_st <erich_st@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2017-06-01 18:54:37 +0000

    fix code for [bugs:#1362] CALL ON ANY raises HALT too late and gives exception
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/main/trunk@11258 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- interpreter/execution/RexxActivation.cpp
+++ interpreter/execution/RexxActivation.cpp
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /* Copyright (c) 1995, 2004 IBM Corporation. All rights reserved.             */
-/* Copyright (c) 2005-2014 Rexx Language Association. All rights reserved.    */
+/* Copyright (c) 2005-2017 Rexx Language Association. All rights reserved.    */
 /*                                                                            */
 /* This program and the accompanying materials are made available under       */
 /* the terms of the Common Public License v1.0 which accompanies this         */
@@ -3648,7 +3648,7 @@ void RexxActivation::processClauseBoundary()
 
     // now set the boundary flag based on whether we still have pending stuff for
     // next go around.
-    clauseBoundary = settings.haveClauseExits() && (conditionQueue == OREF_NULL || conditionQueue->isEmpty());
+    clauseBoundary = settings.haveClauseExits() || !(conditionQueue == OREF_NULL || conditionQueue->isEmpty());
 }
 
 
