a72b093ef693ec31dc2835de4bd240e98c8698ce
erich_st <erich_st@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2017-06-19 17:42:40 +0000

    fix [documentation:#246] Ad native API DecodeConditionInfo
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/main/trunk@11262 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- api/oorexxapi.h
+++ api/oorexxapi.h
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /* Copyright (c) 1995, 2004 IBM Corporation. All rights reserved.             */
-/* Copyright (c) 2005-2014 Rexx Language Association. All rights reserved.    */
+/* Copyright (c) 2005-2017 Rexx Language Association. All rights reserved.    */
 /*                                                                            */
 /* This program and the accompanying materials are made available under       */
 /* the terms of the Common Public License v1.0 which accompanies this         */
@@ -440,7 +440,7 @@ typedef struct
   RexxStringObject message;          // fully filled in message
   RexxStringObject errortext;        // major error text
   RexxStringObject program;          // program name
-  RexxStringObject description;      // program name
+  RexxStringObject description;      // description text
   RexxArrayObject  additional;       // additional information
 } RexxCondition;
 
