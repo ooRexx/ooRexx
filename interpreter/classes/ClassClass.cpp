fbb2499bee3c2ce0568ae32d141bccbe37001463
bigrixx <bigrixx@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2018-06-30 18:51:25 +0000

    [#1541] InstanceMethods does not return enhanced methods
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/main/trunk@11454 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- interpreter/classes/ClassClass.cpp
+++ interpreter/classes/ClassClass.cpp
@@ -1451,9 +1451,9 @@ RexxObject *RexxClass::enhanced(RexxObject **args, size_t argCount)
 
     // create a dummy subclass of the receiver class
     Protected<RexxClass> dummy_subclass = subclass(OREF_NULL, new_string("Enhanced Subclass"), OREF_NULL, OREF_NULL);
-    // create a method dictionary from the collection of methods.  We use dummy subclass for the dictionary scope
-    // source scope.
-    Protected<MethodDictionary> enhanced_instance_mdict = dummy_subclass->createMethodDictionary(enhanced_methods, dummy_subclass);
+    // create a method dictionary from the collection of methods. We use .nil, so that these additional methods will look like
+    // they were added with setMethod
+    Protected<MethodDictionary> enhanced_instance_mdict = dummy_subclass->createMethodDictionary(enhanced_methods, (RexxClass *)TheNilObject);
     // enhance the instance behaviour of the dummy subclass with the new methods
     dummy_subclass->instanceMethodDictionary->merge(enhanced_instance_mdict);
     // and record the changes in behavior
