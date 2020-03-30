fcc20bc92d167acc61ddedfe88bd06db3a7b09d0
erich_st <erich_st@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2020-03-30 18:58:52 +0000

    further code fix for [bugs:#1694] Fix live marking
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/main/trunk@12031 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- interpreter/classes/support/ListContents.cpp
+++ interpreter/classes/support/ListContents.cpp
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /* Copyright (c) 1995, 2004 IBM Corporation. All rights reserved.             */
-/* Copyright (c) 2005-2018 Rexx Language Association. All rights reserved.    */
+/* Copyright (c) 2005-2020 Rexx Language Association. All rights reserved.    */
 /*                                                                            */
 /* This program and the accompanying materials are made available under       */
 /* the terms of the Common Public License v1.0 which accompanies this         */
@@ -208,7 +208,7 @@ void ListContents::flatten(Envelope *envelope)
     // we only mark the active items rather than scanning the entire content area.
     for (size_t position = firstItem; position != NoMore; position = nextEntry(position))
     {
-        memory_mark_general(entries[position].value);
+        flattenRef(entries[position].value);
     }
 
     cleanUpFlatten
