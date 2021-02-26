bf183a84fff44cf56454472d69d67bf046ad9931
erich_st <erich_st@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2021-02-26 21:18:03 +0000

    fix cdode for [bugs:#1748] Typo in faux pthread_mutex_timedlock
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/main/trunk@12183 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- common/platform/unix/SysSemaphore.cpp
+++ common/platform/unix/SysSemaphore.cpp
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /* Copyright (c) 1995, 2004 IBM Corporation. All rights reserved.             */
-/* Copyright (c) 2005-2019 Rexx Language Association. All rights reserved.    */
+/* Copyright (c) 2005-2021 Rexx Language Association. All rights reserved.    */
 /*                                                                            */
 /* This program and the accompanying materials are made available under       */
 /* the terms of the Common Public License v1.0 which accompanies this         */
@@ -95,7 +95,7 @@ bool checkTimeOut(const struct timespec *ts, long &nextInterval)
     if (ts->tv_sec == currentTime.tv_sec)
     {
         // calculate unconditionally, we use the sign to then determine if this was a time out
-        nextInterval = ts->tv_sec - currentTime.tv_sec;
+        nextInterval = ts->tv_nsec - currentTime.tv_nsec;
         if (nextInterval < 0)
         {
             return true;      // a negative result is a timeout
