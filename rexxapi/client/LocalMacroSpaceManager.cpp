9d6427d8f37f6305b597b63a681d8714065dd589
erich_st <erich_st@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2021-11-30 17:31:57 +0000

    fix a SysSaveRexxMacroSpace typo and update Macrospace test group
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/main/trunk@12331 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- rexxapi/client/LocalMacroSpaceManager.cpp
+++ rexxapi/client/LocalMacroSpaceManager.cpp
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /* Copyright (c) 1995, 2004 IBM Corporation. All rights reserved.             */
-/* Copyright (c) 2005-2018 Rexx Language Association. All rights reserved.    */
+/* Copyright (c) 2005-2021 Rexx Language Association. All rights reserved.    */
 /*                                                                            */
 /* This program and the accompanying materials are made available under       */
 /* the terms of the Common Public License v1.0 which accompanies this         */
@@ -372,7 +372,7 @@ RexxReturnCode LocalMacroSpaceManager::saveMacroSpace(const char *target)
     // we're empty, no point in this.
     if (message.parameter1 == 0)
     {
-        return RXMACRO_OK;
+        return RXMACRO_FILE_ERROR;
     }
 
     // now open and write the file header
