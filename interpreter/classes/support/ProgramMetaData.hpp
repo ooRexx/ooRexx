9cd82ed19d0914e810c5aba8c5502a073b069ae0
orexx <orexx@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2022-01-26 11:12:46 +0000

    Internal form changed with [r12326], therefore increasing METAVERSION (image version).
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/main/trunk@12349 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- interpreter/classes/support/ProgramMetaData.hpp
+++ interpreter/classes/support/ProgramMetaData.hpp
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /* Copyright (c) 1995, 2004 IBM Corporation. All rights reserved.             */
-/* Copyright (c) 2005-2019 Rexx Language Association. All rights reserved.    */
+/* Copyright (c) 2005-2022 Rexx Language Association. All rights reserved.    */
 /*                                                                            */
 /* This program and the accompanying materials are made available under       */
 /* the terms of the Common Public License v1.0 which accompanies this         */
@@ -71,7 +71,7 @@ protected:
     enum
     {
         MAGICNUMBER = 11111,           // remains constant from release-to-release
-        METAVERSION = 42               // gets updated when internal form changes
+        METAVERSION = 43               // gets updated when internal form changes
     };
 
 
