6d7dffb9f6ad82e957a23d1da7c160107fa96cc8
erich_st <erich_st@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2022-09-03 19:01:47 +0000

    fix oorexxapi.h typo; update rexxapi for [feature-requests:#808] Add an API to obtain the interpreter instance context; add minor clarifications to RXCMD exit and ioRedirection methods
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/main/trunk@12507 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- api/oorexxapi.h
+++ api/oorexxapi.h
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /* Copyright (c) 1995, 2004 IBM Corporation. All rights reserved.             */
-/* Copyright (c) 2005-2021 Rexx Language Association. All rights reserved.    */
+/* Copyright (c) 2005-2022 Rexx Language Association. All rights reserved.    */
 /*                                                                            */
 /* This program and the accompanying materials are made available under       */
 /* the terms of the Common Public License v1.0 which accompanies this         */
@@ -2380,7 +2380,7 @@ struct RexxMethodContext_
      {
          threadContext->ClearCondition();
      }
-     RexxInstance* GetInterpreterIntsance()
+     RexxInstance* GetInterpreterInstance()
      {
          return threadContext->GetInterpreterInstance();
      }
@@ -4060,7 +4060,7 @@ struct RexxExitContext_
      {
          threadContext->ClearCondition();
      }
-     RexxInstance* GetInterpreterIntsance()
+     RexxInstance* GetInterpreterInstance()
      {
          return threadContext->GetInterpreterInstance();
      }
