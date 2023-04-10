8554b40cc92f0a9dd04b93fc085dca41f2c3198d
erich_st <erich_st@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2023-04-10 16:37:43 +0000

    fix code and add tests for [bugs:#1887] Ticker fails when cancelled twice
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/main/trunk@12668 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- interpreter/platform/windows/TimeSupport.cpp
+++ interpreter/platform/windows/TimeSupport.cpp
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /* Copyright (c) 1995, 2004 IBM Corporation. All rights reserved.             */
-/* Copyright (c) 2005-2021 Rexx Language Association. All rights reserved.    */
+/* Copyright (c) 2005-2023 Rexx Language Association. All rights reserved.    */
 /*                                                                            */
 /* This program and the accompanying materials are made available under       */
 /* the terms of the Common Public License v1.0 which accompanies this         */
@@ -396,7 +396,6 @@ RexxMethod0(int, ticker_createTimer)
 
     // store this in state variables
     context->SetObjectVariable("EVENTSEMHANDLE", context->NewPointer(SemHandle));
-    context->SetObjectVariable("TIMERSTARTED", context->True());
 
     return 0;
 }
