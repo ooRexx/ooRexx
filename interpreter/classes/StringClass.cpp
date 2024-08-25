d94bd5e4b8f7b9e99e1108fcdcfc2eae65cb731c
erich_st <erich_st@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2024-08-25 20:04:35 +0000

    fix code, update rexxref and add test for [bugs:#1951] Error message insert masking issues
    git-svn-id: https://svn.code.sf.net/p/oorexx/code-0/main/trunk@12882 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- interpreter/classes/StringClass.cpp
+++ interpreter/classes/StringClass.cpp
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /* Copyright (c) 1995, 2004 IBM Corporation. All rights reserved.             */
-/* Copyright (c) 2005-2021 Rexx Language Association. All rights reserved.    */
+/* Copyright (c) 2005-2024 Rexx Language Association. All rights reserved.    */
 /*                                                                            */
 /* This program and the accompanying materials are made available under       */
 /* the terms of the Common Public License v1.0 which accompanies this         */
@@ -1638,7 +1638,7 @@ RexxString *RexxString::stringTrace()
     // loop through, breaking on the first non-displayable character
     for (; i > 0; i--, current++)
     {
-        if (*current < ch_SPACE && *current != ch_TAB)
+        if (*current < ch_SPACE && *current != ch_TAB && *current != '\r' && *current != '\n')
         {
             nonDisplay = true;
             break;
@@ -1653,14 +1653,14 @@ RexxString *RexxString::stringTrace()
 
     RexxString *newCopy = (RexxString *)copy();
     i = newCopy->getLength();
-    char *outptr = newCopy->getWritableData();
+    unsigned char *outptr = (unsigned char *)(newCopy->getWritableData());
 
     // now loop again translating all of the non-displayables
     for (; i > 0; i--)
     {
-        // we don't translate tabs either...all other
+        // we don't translate cr, lf and tabs...all other
         // non-displayables become question marks.
-        if (*outptr < ch_SPACE && *outptr != ch_TAB)
+        if (*outptr < ch_SPACE && *outptr != ch_TAB && *outptr != '\r' && *outptr != '\n')
         {
             *outptr = '?';
         }
